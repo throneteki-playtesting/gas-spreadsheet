2f5c5099b6065cf8676e3e617f4660396808063a
Stephen Patane <patane97@gmail.com>
2024-09-12 19:53:12 +1000

    Fixing render text scaling

--- public/js/Render.js
+++ public/js/Render.js
@@ -7,7 +7,7 @@ function autoSize() {
     while (fontSize >= minimumSize && item.scrollHeight > item.clientHeight || item.scrollWidth > item.clientWidth) {
       for (const child of [item, ...item.children]) {
         fontSize = parseFloat(window.getComputedStyle(child, null).getPropertyValue("font-size"), 10);
-        fontSize -= 1;
+        fontSize -= 0.1;
         child.style.fontSize = fontSize + "px";
       }
     }
